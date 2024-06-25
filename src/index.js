cfd1d6fb679125c86093bdd0b4dd64d85d28e857
x-xx-xxx-xxxx <47820011+x-xx-xxx-xxxx@users.noreply.github.com>
2024-06-25 14:42:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.hackserver.net": dockerHub,
+  "quay.hackserver.net": "https://quay.io",
+  "gcr.hackserver.net": "https://gcr.io",
+  "k8s-gcr.hackserver.net": "https://k8s.gcr.io",
+  "k8s.hackserver.net": "https://registry.k8s.io",
+  "ghcr.hackserver.net": "https://ghcr.io",
+  "cloudsmith.hackserver.net": "https://docker.cloudsmith.io",
+  "ecr.hackserver.net": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.hackserver.net": dockerHub,
 };
 
 function routeByHosts(host) {
